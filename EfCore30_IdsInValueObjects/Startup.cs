cdd1608f7e090335a170c612d229a4667ac243dd
Georg Mayer <georg.mayer@kpark.de>
2019-05-31 18:08:56 +0200

    upgraded all nuget packes to 3.0 and added ef core 3.0 and sqlite 3.0

--- EfCore30_IdsInValueObjects/Startup.cs
+++ EfCore30_IdsInValueObjects/Startup.cs
@@ -30,8 +30,8 @@ namespace EfCore30_IdsInValueObjects
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc(o => o.EnableEndpointRouting = false)
+                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
