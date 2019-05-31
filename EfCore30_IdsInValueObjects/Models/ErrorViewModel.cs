c672b14d0be3d251f6e88f73891d65f0faa142d8
Georg Mayer <georg.mayer@kpark.de>
2019-05-31 17:45:04 +0200

    add project template for .net core MVC

---
+++ EfCore30_IdsInValueObjects/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EfCore30_IdsInValueObjects.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
